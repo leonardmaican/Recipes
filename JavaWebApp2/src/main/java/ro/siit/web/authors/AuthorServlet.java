519aa3febd0878a6345de33906804ce83f9bd75a
leonardmaican <leonard.maican@yahoo.ro>
2020-04-30 13:12:40 +0300

    The final project recipes app commit

--- JavaWebApp2/src/main/java/ro/siit/web/authors/AuthorServlet.java
+++ JavaWebApp2/src/main/java/ro/siit/web/authors/AuthorServlet.java
@@ -1,4 +1,18 @@
 package ro.siit.web.authors;
 
-public class AuthorServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class AuthorServlet extends HttpServlet {
+    private AuthorsDAO authorsDAO = new AuthorsDAO();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        List<Author> allAuthors= authorsDAO.getAllAuthors();
+
+
+    }
 }
